8cf3349f6021137086070e72fed57f76b5a7f346
ixunio <38986504+ixunio@users.noreply.github.com>
2025-12-22 02:48:01 +0100

    chore: declare importable from package

--- superjwt/__init__.py
+++ superjwt/__init__.py
@@ -1,6 +1,18 @@
 from superjwt._version import __version__
+from superjwt.definitions import JOSEHeader, JWTClaims, JWTDatetime
 from superjwt.jwt import JWT
 
+
+__all__ = [
+    "JWT",
+    "JOSEHeader",
+    "JWTClaims",
+    "JWTDatetime",
+    "__version__",
+    "decode",
+    "encode",
+    "inspect",
+]
 
 _local_jwt_instance = JWT()
 
